550435c383ab4eddc0b261c2667d068be907098f
Tejasvi S Jain <tejasvisjain0214@gmail.com>
2024-05-14 23:06:53 +0530

    added a splash screen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import About from "./components/About";
-import Home from "./components/Home";
-import Navbar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
-import Experience from "./components/Experience";
-import Contact from "./components/Contact";
-import PostmanComponent from "./components/Postman";
-import Education from "./components/Education";
 import { Analytics } from "@vercel/analytics/react";
+import { useState, useEffect } from "react";
+import Base from "./components/Base";
+import "./components/styles/splash.css"; // Import the CSS file
+import Splashscreen from "./components/Splash";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+  const [timerFinished, setTimerFinished] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimerFinished(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []); // Run once on component mount
+
+  useEffect(() => {
+    if (timerFinished) {
+      const slideOutTimer = setTimeout(() => {
+        setShowSplash(false);
+      }, 500); // Adjust the duration to match the slide-out animation duration
+      return () => clearTimeout(slideOutTimer);
+    }
+  }, [timerFinished]); // Run when timerFinished changes
+
   return (
     <div className="App">
       <Analytics />
-      <Navbar />
-      <Home />
-      <About />
-      <Portfolio />
-      <PostmanComponent />
-      <Experience />
-      {/* <Education /> */}
-      <Contact />
-      <SocialLinks />
+      {showSplash && <Splashscreen slideOut={timerFinished} />} {/* Pass slideOut prop */}
+      <Base />
     </div>
   );
 }
